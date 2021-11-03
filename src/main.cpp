2be0e5dce646a405f7eda386527ff9ced5b13ded
vchavauty <v.chavauty@gmail.com>
2021-11-03 08:46:01 -0300

    minor

--- src/main.cpp
+++ src/main.cpp
@@ -15,31 +15,26 @@ int main(int argc, char **argv)
     Ame::AmeConfig config;
     Ame::AmeLibrary<Ame::Word> lib(config);
 
+
+    // Loading the parser directly!
+    
     lib.loadParserManually(Ame::JMdict::getInformation);
     Ame::Word word;
     Ame::ame_result o = lib.invokeParser(word, "/home/vchavauty/Documents/Code/AmeKanji/Repository/vocab/JMdict.xml", {"開ける"});
     std::cout << "Result: " << o.Message << std::endl;
     std::cout << word;
-    o = lib.invokeParser(word, "/home/vchavauty/Documents/Code/AmeKanji/Repository/vocab/JMdict.xml", {"開ける", "ひらける"});
-    std::cout << "Result: " << o.Message << std::endl;
-    std::cout << word; 
-   
     
-    Ame::CardMode cm = lib.getCardMode();
-    std::string cm_str = (cm == Ame::CardMode::Kanji) ? "Kanji" :
-                    ((cm == Ame::CardMode::Word) ? "Word" : "Unknown");
-
-    std::cout << "Card Mode: " << cm_str << std::endl;
-
+    // Loading the parser automatically through the configuration, but sending the dictionary manually
     config.Parser = "JMdict";
     Ame::AmeLibrary<Ame::Word> newlib(config);
-    
     o = newlib.loadParserAutomatically();
     newlib.setDictionaryManually("/home/vchavauty/Documents/Code/AmeKanji/Repository/vocab/JMdict.xml", "file");
     newlib.invokeParser(word, {"試みる"}, {}, true);
     std::cout << word;
 
+    // Loading everything through the configuration
 
+    
     Ame::AmeLibrary<Ame::Word> newnewlib(config);
     config.ConfigurationFile = "/home/vchavauty/Documents/Code/AmeKanji/Repository/vocab/JMdict.xml";
     newnewlib.setDictionaryAutomatically();
