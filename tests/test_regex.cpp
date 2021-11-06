9726bedd0f19218bb919101c9447d4b31a645a0d
lesserfish <v.chavauty@gmail.com>
2021-11-06 03:51:36 -0300

    more Regex Match tests

--- tests/test_regex.cpp
+++ tests/test_regex.cpp
@@ -12,9 +12,8 @@ TEST(CoreLibrary, ParseRegexReplace)
     EXPECT_STREQ(out[0].c_str(), "asdyoloasd");
 } 
 
-TEST(CoreLibrary, ParseRegexMatch)
+TEST(CoreLibrary, ParseRegexMatch_1)
 {
-    //TODO
     Core::RegexInstruction i1(Core::regOP::MATCH, "sub(\\w*)a", "");
     Core::RegexOrder o1(i1);
     std::vector<std::string> out = Core::ParseRegex(o1, "submarine subcars submama subhuman metropolos subcat konosuba");
@@ -26,4 +25,30 @@ TEST(CoreLibrary, ParseRegexMatch)
     EXPECT_STREQ(out[3].c_str(), "subhuma");
     EXPECT_STREQ(out[4].c_str(), "subca");
     EXPECT_STREQ(out[5].c_str(), "suba");
+} 
+
+TEST(CoreLibrary, ParseRegexMatch_2)
+{
+    Core::RegexInstruction i1(Core::regOP::MATCH, "sub(\\w*?)a", "");
+    Core::RegexOrder o1(i1);
+    std::vector<std::string> out = Core::ParseRegex(o1, "submarine subcars submama subhuman metropolos subcat konosuba");
+
+    EXPECT_EQ(out.size(), 6);
+    EXPECT_STREQ(out[0].c_str(), "subma");
+    EXPECT_STREQ(out[1].c_str(), "subca");
+    EXPECT_STREQ(out[2].c_str(), "subma");
+    EXPECT_STREQ(out[3].c_str(), "subhuma");
+    EXPECT_STREQ(out[4].c_str(), "subca");
+    EXPECT_STREQ(out[5].c_str(), "suba");
+} 
+TEST(CoreLibrary, ParseRegexMatch_3)
+{
+    Core::RegexInstruction i1(Core::regOP::MATCH, "[A-Z](\\w+)", "");
+    Core::RegexOrder o1(i1);
+    std::vector<std::string> out = Core::ParseRegex(o1, "Well, if it isn't Harry Potter!");
+
+    EXPECT_EQ(out.size(), 3);
+    EXPECT_STREQ(out[0].c_str(), "Well");
+    EXPECT_STREQ(out[1].c_str(), "Harry");
+    EXPECT_STREQ(out[2].c_str(), "Potter");
 } 
